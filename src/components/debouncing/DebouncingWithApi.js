4e4f9730c13f0af82d689f7589900065c5e0a398
tarun13rathore <tarun13rathore@gmail.com>
2023-12-19 09:44:30 +0530

    ui fix

--- src/components/debouncing/DebouncingWithApi.js
+++ src/components/debouncing/DebouncingWithApi.js
@@ -26,17 +26,51 @@ export default function DebouncingWithAPi() {
   }, [searchTerm]);
 
   return (
-    <div>
-      <input
-        type="text"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
-      <div>
-        {name?.map((photo) => (
-          <div>{photo.name}</div>
-        ))}
+    <>
+      <div style={{ marginTop: "150px" }}>
+        <div
+          style={{
+            border: "1px solid black",
+            textAlign: "center",
+            marginTop: "50px",
+            width: "300px", // Set a fixed width for the container
+            margin: "auto", // Center the container horizontally
+            padding: "20px", // Add some padding for spacing
+            borderRadius: "8px", // Add rounded corners
+            boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)", // Add a subtle box shadow
+          }}
+        >
+          <input
+            type="text"
+            value={searchTerm}
+            placeholder="Search Name"
+            style={{
+              border: "1px solid black",
+              marginBottom: "10px", // Add some space between the input and the card content
+              width: "100%", // Make the input take up the full width
+              padding: "8px", // Add padding to the input
+              boxSizing: "border-box", // Ensure padding is included in the width
+            }}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <div>
+            {name?.map((photo) => (
+              <div
+                key={photo.id} // Add a unique key for each card
+                style={{
+                  border: "1px solid #ddd",
+                  borderRadius: "8px",
+                  padding: "10px",
+                  marginBottom: "10px",
+                  backgroundColor: "#f9f9f9", // Set a background color
+                }}
+              >
+                {photo.name}
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
